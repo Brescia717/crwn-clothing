b9d30076a14c851e37b909b97b74bb17ce064c3f
Paul Brescia <brescia717@gmail.com>
2022-12-04 20:09:25 -0500

    Set up categories and Category component:
    * Created `categories` list.
    * Created `Category` component.
    * Added `sass` package from yarn.

---
+++ src/components/category.component.jsx
@@ -0,0 +1,15 @@
+import './category.styles.scss';
+
+const Category = ({title}) => {
+  return (
+    <div className="category-container">
+      <div className="background-image"></div>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  )
+}
+
+export default Category;
