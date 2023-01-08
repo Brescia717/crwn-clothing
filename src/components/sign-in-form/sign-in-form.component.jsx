29cbe956c081043a1472953e9c0fa88c3642f976
Paul Brescia <brescia717@gmail.com>
2023-01-08 14:32:02 -0500

    Add to cart feature
    * Added addToCart feature.
    * Added tally of all items in cart.

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.component";
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
 
 import {
   signInAuthUserWithEmailAndPassword,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
+} from '../../utils/firebase/firebase.utils';
 
-import "./sign-in-form.styles.scss";
+import './sign-in-form.styles.scss';
 
 const defaultFormFields = {
-  email: "",
-  password: "",
+  email: '',
+  password: '',
 };
 
 const SignInForm = () => {
@@ -39,11 +39,11 @@ const SignInForm = () => {
       resetFormFields();
     } catch (error) {
       switch (error.code) {
-        case "auth/wrong-password":
-          alert("incorrect password for email");
+        case 'auth/wrong-password':
+          alert('incorrect password for email');
           break;
-        case "auth/user-not-found":
-          alert("user not found");
+        case 'auth/user-not-found':
+          alert('user not found');
           break;
         default:
           console.log(error);
@@ -58,37 +58,37 @@ const SignInForm = () => {
 
   return (
     <>
-      <div className="sign-up-container">
+      <div className='sign-up-container'>
         <h2>Already have an account?</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={handleSubmit}>
           <FormInput
-            label="Email"
+            label='Email'
             inputOptions={{
-              type: "email",
+              type: 'email',
               required: true,
               onChange: handleChange,
-              name: "email",
+              name: 'email',
               value: email,
             }}
           />
 
           <FormInput
-            label="Password"
+            label='Password'
             inputOptions={{
-              type: "password",
+              type: 'password',
               required: true,
               onChange: handleChange,
-              name: "password",
+              name: 'password',
               value: password,
             }}
           />
 
-          <div className="buttons-container">
-            <Button type="submit">Sign In</Button>
+          <div className='buttons-container'>
+            <Button type='submit'>Sign In</Button>
             <Button
-              type="button"
-              buttonType="google"
+              type='button'
+              buttonType='google'
               onClick={signInWithGoogle}
             >
               Google sign in
